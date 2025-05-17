00a436ab86197eca9dcef75786625d3ba6daf3e6
Song Dokyun <dokrious0717@gmail.com>
2025-05-17 13:47:33 +0900

    Study 페이지 라우팅 및 작성/수정 기능 구현

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
@@ -9,6 +10,9 @@ function Header() {
         <div className="button-container">
           <button className="login">로그인</button>
           <button className="join">회원가입</button>
+           <Link to="/study/write">
+            <button className="write">모집글 작성하기</button>
+          </Link>
         </div>
       </div>
     </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StudyWritePage from "./pages/StudyWritePage";
+import StudyEditPage from "./pages/StudyEditPage";
 
 function Main() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/study/write" element={<StudyWritePage />} />
+      <Route path="/study/edit" element={<StudyEditPage />} />
       <Route />
     </Routes>
     </BrowserRouter>
