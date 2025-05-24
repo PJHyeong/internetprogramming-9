5a048468d5edd1626c1bbd9428d190bd192128d2
Song Dokyun <dokrious0717@gmail.com>
2025-05-24 12:54:08 +0900

    인영 브랜치 기반 기능 복사 반영

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudyWritePage from "./pages/StudyWritePage";
 import StudyEditPage from "./pages/StudyEditPage";
+import Home from "./pages/Home";
 
 function Main() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Home />}/>
       <Route path="/study/write" element={<StudyWritePage />} />
       <Route path="/study/edit" element={<StudyEditPage />} />
       <Route />
