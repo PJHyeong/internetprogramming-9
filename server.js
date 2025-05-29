25a5e474e3206ee2d2cb63c48339275fe269e316
inyouxg <dolley03@naver.com>
2025-05-29 18:10:51 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const loginRoutes = require('./routes/login'); 
 require('dotenv').config(); 
 
 const app = express();
+
+//CORS 미들웨어 추가
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 //미들웨어
 app.use(express.json());
@@ -11,7 +18,6 @@ app.use(cookieParser());
 
 //라우터 
 app.use('/api', loginRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
